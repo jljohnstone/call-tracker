1decab795fdac1d51a2f5374421e2404d3e6a1a8
Jason Johnstone <jason@johnstonemedia.com>
2023-02-25 15:00:23 -0600

    style login form; update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  get '/notes', to: redirect("/")
   resources :users
   resources :notes
   resources :customers
 
+
   constraints Clearance::Constraints::SignedOut.new do
-    root to: "clearance/sessions#new"
+    root to: "clearance/sessions#new", as: :signed_out_root
   end
 
-  # root "pages#index"
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "notes#index", as: :signed_in_root
+  end
+
   get "/settings", to: "pages#settings"
   get "/search", to: "pages#search"
+  root "pages#index"
 end
