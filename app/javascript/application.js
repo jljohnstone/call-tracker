bdbd64158be15faa557ac6c80d7cf9eb687d64b4
Jason Johnstone <jason@johnstonemedia.com>
2023-02-28 13:23:15 -0600

    add get notifications button on settings view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,19 +8,41 @@ import "controllers"
 //   })
 // }
 
-// const button = document.getElementById("notifications")
-// button.addEventListener("click", () => {
+// const registerServiceWorker = async () => {
+//   if ("serviceWorker" in navigator) {
+//     try {
+//       const registration = await navigator.serviceWorker.register("/service-worker.js", {
+//         scope: "/",
+//       });
+//       if (registration.installing) {
+//         console.log("Service worker installing")
+//       } else if (registration.waiting) {
+//         console.log("Service worker installed")
+//       } else if (registration.active) {
+//         console.log("Service worker active")
+//       }
+//     } catch (error) {
+//       console.error(`Registration failed with ${error}`)
+//     }
+//   }
+// }
+
+// registerServiceWorker()
+
+const getNotificationsBtn = document.getElementById("get-notifications")
+getNotificationsBtn.addEventListener("click", () => {
   Notification.requestPermission().then((result) => {
     if (result === "granted") {
       console.log("notification permission granted")
       testNotification()
     }
   })
-// })
+})
+
 
 function testNotification() {
   console.log("sending notification...")
   new Notification("Notification Title", { body: "This is the body.", icon: "android-icon-192x192.png"})
   console.log("...notifcation sent")
-  setTimeout(testNotification, 30000)
+  // setTimeout(testNotification, 30000)
 }
