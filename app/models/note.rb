33429d06616f0f8fb24c445bd3063d8bb67e7840
Jason Johnstone <jason@johnstonemedia.com>
2023-03-02 16:33:03 -0600

    add turbo features to new note form and some customer views

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,4 +4,11 @@ class Note < ApplicationRecord
   belongs_to :customer
   belongs_to :user, optional: true
   validates :content, presence: true
+
+  # after_create_commit -> {
+  #   broadcast_prepend_to "notes",
+  #   partial: "notes/note",
+  #   locals: { note: self },
+  #   target: "notes"
+  # }
 end
