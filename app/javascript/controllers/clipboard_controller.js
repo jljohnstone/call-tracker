42018e2099b39f6f1616bf539d6f9f40f081c5d6
Jason Johnstone <jason@johnstonemedia.com>
2023-03-06 16:52:51 -0600

    add copy number button to note form

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,10 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "source" ]
+  static targets = [ "source", "tooltip" ]
 
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.value)
+    this.tooltipTarget.classList.add("tooltip-open")
   }
 
 }
